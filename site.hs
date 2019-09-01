813d6849828146579abea8befd61d4ede5569cbe
leigh-perry <lperry.breakpoint@gmail.com>
2019-09-01 17:56:39 +1000

    develop: Start the content

--- site.hs
+++ site.hs
@@ -1,102 +1,68 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+
 import           Data.Monoid (mappend)
 import           Hakyll
 
-
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
+main =
+  hakyll $ do
     match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
+      route idRoute
+      compile copyFileCompiler
     match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
-
+      route idRoute
+      compile compressCssCompiler
     match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
-{-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
--}
+      route $ setExtension "html"
+      compile $ pandocCompiler >>= loadAndApplyTemplate "templates/default.html" defaultContext >>= relativizeUrls
     match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
+      route $ setExtension "html"
+      compile $
+        pandocCompiler >>= loadAndApplyTemplate "templates/post.html" postCtx >>= saveSnapshot "content" >>=
+        loadAndApplyTemplate "templates/default.html" postCtx >>=
+        relativizeUrls
     create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let archiveCtx =
+              listField "posts" postCtx (return posts) `mappend` constField "title" "Archives" `mappend` defaultContext
+        makeItem "" >>= loadAndApplyTemplate "templates/archive.html" archiveCtx >>=
+          loadAndApplyTemplate "templates/default.html" archiveCtx >>=
+          relativizeUrls
     match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let indexCtx =
+              listField "posts" postCtx (return posts) `mappend` constField "title" "Home" `mappend` defaultContext
+        getResourceBody >>= applyAsTemplate indexCtx >>= loadAndApplyTemplate "templates/default.html" indexCtx >>=
+          relativizeUrls
     match "templates/*" $ compile templateCompiler
-    create ["atom.xml"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = postCtx `mappend`
-                    constField "description" "This is the post description"
-
-            posts <- fmap (take 10) . recentFirst =<< loadAll "posts/*"
-            renderAtom myFeedConfiguration feedCtx posts
-
-    match "templates/*" $ compile templateCompiler
-    create ["feed.xml"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = postCtx `mappend`
-                    constField "description" "This is the post description"
-
-            posts <- fmap (take 10) . recentFirst =<< loadAll "posts/*"
-            renderRss myFeedConfiguration feedCtx posts
-
+    feed "atom.xml" renderAtom
+    feed "feed.xml" renderRss
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+postCtx = dateField "date" "%B %e, %Y" `mappend` defaultContext
 
 myFeedConfiguration :: FeedConfiguration
-myFeedConfiguration = FeedConfiguration
-    { feedTitle       = "Leigh Perry's Blog"
-    , feedDescription = "Leigh Perry's blog"
-    , feedAuthorName  = "Leigh Perry"
+myFeedConfiguration =
+  FeedConfiguration
+    { feedTitle = "Leigh Perry's Blog"
+    , feedDescription = "Leigh Perry's software engineering blog"
+    , feedAuthorName = "Leigh Perry"
     , feedAuthorEmail = "lperry.breakpoint@gmail.com"
-    , feedRoot        = "https://leigh-perry.github.io"
+    , feedRoot = "https://leigh-perry.github.io"
     }
+
+feed :: Identifier -> (FeedConfiguration -> Context String -> [Item String] -> Compiler (Item String)) -> Rules ()
+feed filename renderer =
+  create [filename] $ do
+    route idRoute
+    compile $ do
+      let feedCtx = postCtx `mappend` constField "description" "Another post from Leigh"
+      posts <- fmap (take 10) . recentFirst =<< loadAll "posts/*"
+      renderer myFeedConfiguration feedCtx posts
