6fd80bee910ab4f02f350f4aa763abf193de38d8
leigh-perry <lperry.breakpoint@gmail.com>
2019-09-02 21:35:41 +1000

    develop: New content

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
 pause() {
+  read -n1 -r -p "Press space/enter to continue, any other key to cancel..." key    
   if [ "$key" = '' ]; then
-      echo "Cancelled"
-      exit 1
+    echo ""
+  else
+    echo "Cancelled"
+    exit 1
   fi
 }
 
 gqc New content
 stack build ; stack exec blog clean ; stack exec blog build
 pause
+echo continue
 
-#git fetch --all ; git checkout -b master --track origin/master
-#
-#cp -a _site/. .
-#git config user.name "leigh-perry" ; git config user.email "lperry.breakpoint@gmail.com" ; gs ; gqc "Publish"
-#git push origin master:master
-#git checkout develop ; git branch -D master
-#git push
+git fetch --all ; git checkout -b master --track origin/master
+pause
+
+cp -a _site/. .
+git config user.name "leigh-perry" ; git config user.email "lperry.breakpoint@gmail.com" ; gs ; gqc "Publish"
+git push origin master:master
+pause
+
+git checkout develop ; git branch -D master
+pause
+
+git push
